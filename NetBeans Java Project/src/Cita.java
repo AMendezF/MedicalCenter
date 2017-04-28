82769c17fe4b15a6a2145d28d3d17e1d8a281591
AMendezF <a.mendezworks@gmail.com>
2017-04-28 21:47:55 +0200

    Added more
    Cita:
    -public String mostrarEspecialidades(Connection connection) throws
    SQLException
    
    Gestor:
    -Modificados cabeceras de métodos.

--- NetBeans Java Project/src/Cita.java
+++ NetBeans Java Project/src/Cita.java
@@ -1,35 +1,70 @@
+﻿
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Alejandro
  */
 public class Cita {
+
     private String fecha;
     private String idPaciente;
-    private String idMedico;
     private String especialidad;
+    private String idMedico;
 
     /**
      * Constructor de cita.
+     *
      * @param fecha Formato fecha yyyy/mm/dd.
      * @param idPaciente Identificador de paciente.
-     * @param idMedico Identificador médico.
      * @param especialidad Campo especialidad de la cita.
+     * @param idMedico Identificador médico.
      */
     public Cita(String fecha, String idPaciente, String idMedico, String especialidad) {
         this.setFecha(fecha);
         this.setIdPaciente(idPaciente);
-        this.setIdMedico(idMedico);
         this.setEspecialidad(especialidad);
+        this.setIdMedico(idMedico);
     }
-    
+
+    /**
+     * Muestra las especialidades disponibles para solicitar en la cita.
+     * @param connection Objeto para poder realizar consulta a la base de datos.
+     * @return Retorna un string con todas las especialidades.
+     * @throws SQLException 
+     */
+    public String mostrarEspecialidades(Connection connection) throws SQLException {
+        String resultado = "";
+        String sql;
+        ResultSet resultSet;
+        
+        sql = "SELECT * FROM centromedico.especialidad ORDER BY cod_especialidad;";
+        resultSet = makeQuery(connection, sql);
+
+        while (resultSet.next()) {
+            resultado += Integer.toString(resultSet.getInt("cod_especialidad"))
+                    + ". " + resultSet.getString("nombre") + "\n";
+        }
+        return resultado;
+    }
+
+    public ResultSet makeQuery(Connection connection, String sql) throws SQLException {
+        PreparedStatement preparedStmt = connection.prepareStatement(sql);
+        ResultSet resulSet = preparedStmt.executeQuery();
+        return resulSet;
+    }
+
     /**
      * Devuelve la fecha de la cita.
+     *
      * @return Retorna la fecha en formato yyyy/mm/dd.
      */
     public String getFecha() {
@@ -38,6 +73,7 @@ public class Cita {
 
     /**
      * Devuelve el identificador del paciente de la cita.
+     *
      * @return Retorna el identificador.
      */
     public String getIdPaciente() {
@@ -45,7 +81,17 @@ public class Cita {
     }
 
     /**
+     * Devuelve la especialidad para la que se ha pedido la cita.
+     *
+     * @return Retorna la especialidad.
+     */
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    /**
      * Devuelve el identificador del médico de la cita.
+     *
      * @return Retorna el identificador.
      */
     public String getIdMedico() {
@@ -53,15 +99,8 @@ public class Cita {
     }
 
     /**
-     * Devuelve la especialidad para la que se ha pedido la cita.
-     * @return Retorna la especialidad.
-     */
-    public String getEspecialidad() {
-        return especialidad;
-    }
-    
-    /**
      * Fija la fecha de la cita.
+     *
      * @param fecha Formato yyyy/mm/dd.
      */
     public void setFecha(String fecha) {
@@ -70,6 +109,7 @@ public class Cita {
 
     /**
      * Fija el identificador del paciente.
+     *
      * @param idPaciente Identificador de paciente.
      */
     public void setIdPaciente(String idPaciente) {
@@ -77,19 +117,29 @@ public class Cita {
     }
 
     /**
+     * Fija la especialidad de la cita.
+     *
+     * @param especialidad Campo especialidad de la cita.
+     */
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    /**
      * Fija el identificador del médico.
+     *
      * @param idMedico Identificador de médico.
      */
     public void setIdMedico(String idMedico) {
         this.idMedico = idMedico;
     }
 
-    /**
-     * Fija la especialidad de la cita.
-     * @param especialidad Campo especialidad de la cita.
-     */
-    public void setEspecialidad(String especialidad) {
-        this.especialidad = especialidad;
+    @Override
+    public String toString() {
+        return "Cita{" + "fecha=" + fecha
+                + ", idPaciente=" + idPaciente
+                + ", especialidad=" + especialidad
+                + ", idMedico=" + idMedico + '}';
     }
-    
+
 }
--- NetBeans Java Project/src/Gestor.java
+++ NetBeans Java Project/src/Gestor.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,76 +9,83 @@
  * @author Alejandro
  */
 public class Gestor {
+
     private Conexion conexion;
 
     /**
      * Constructor de gestor.
+     *
      * @param conexion Llave para comunicar con la DB.
      */
     public Gestor(Conexion conexion) {
         this.conexion = conexion;
     }
-    
+
     /**
      * Inserta un nuevo médico en la base de datos.
+     * @param medico Médico que se desea añadir a la BD.
      * @return true = añadido a la DB, false = fallo al añadir
      */
-    public boolean addMedico () {
+    public boolean addMedico(Medico medico) {
         boolean added = false;
-        
+
         return added;
     }
-    
+
     /**
      * Inserta un nuevo paciente en la base de datos.
+     * @param paciente Paciente que se desea añadir a la BD.
      * @return true = añadido a la DB, false = fallo al añadir
      */
-    public boolean addPaciente () {
+    public boolean addPaciente(Paciente paciente) {
         boolean added = false;
-        
+
         return added;
     }
-    
+
     /**
      * Sustituye un médico por otro.
+     * @param medico Médico al que se pretende reemplazar.
+     * @param sustituto Médico que reemplaza al anterior.
      * @return true = eliminado, false = fallo al eliminar
      */
-    public boolean sustituirMedico () {
+    public boolean sustituirMedico(Medico medico, Medico sustituto) {
         boolean eliminado = false;
-        
+
         return eliminado;
     }
-    
+
     /**
-     * Muestra las citas de un día organizadas por horario y
-     * salas de especialidad.
-     * @return Resumen de todas las citas del día
-     * por salas (especialidades).
+     * Muestra las citas de un día organizadas por horario y salas de
+     * especialidad.
+     *
+     * @return Resumen de todas las citas del día por salas (especialidades).
      */
-    public String mostrarItinerario () {
+    public String mostrarItinerario() {
         String itinerario = "";
-        
+
         return itinerario;
     }
-    
+
     /**
      * Muestra todos los pacientes que contiene la base de datos.
+     *
      * @return Devuelve todos los pacientes con DNI, nombre, apellidos...
      */
-    public String mostrarPacientes () {
+    public String mostrarPacientes() {
         String lista = "";
-        
+
         return lista;
     }
-    
+
     /**
      * Elimina una cita de un paciente.
-     * @param paciente
+     * @param paciente Paciente del que se desea borrar una cita.
      * @return true = eliminado, false = fallo al eliminar
      */
-    public boolean eliminarCita (Paciente paciente) {
+    public boolean eliminarCita(Paciente paciente) {
         boolean eliminado = false;
-        
+
         return eliminado;
     }
 }
